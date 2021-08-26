4fcc5fe1e75fd40ef1ace860b3bd6a6931ff7916
Gökhan Yalnız <gokhan.yalniz@ist.ac.at>
2021-08-26 12:17:56 +0200

    fixed dnscontinue to use the time corresponding to the last state in the case of adaptive time stepping.

--- python/dnscontinue.py
+++ python/dnscontinue.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 import argparse
-from pathlib import Path
-import os
-import numpy as np
-from pyvista.plotting.renderer import CameraPosition
-import dns
-import glob
 import re
+from pathlib import Path
 from subprocess import call
+import numpy as np
+
+import dns
 
 
 def main():
@@ -24,51 +22,52 @@ def main():
 
     args = vars(parser.parse_args())
 
-    print(args)
-
     dnscontinue(**args)
 
 
 def dnscontinue(rundir, script):
 
     rundir = Path(rundir)
-    os.chdir(rundir)
 
-    states = list(glob.glob('state.*'))
-    i_final_state = 0 
-    for state in states:
-        i_state = int(state[-6:])
-        if i_state  > i_final_state:
-            i_final_state = i_state
-    
-    parameters = dns.readParameters("parameters.in")
-    parameters['initiation']['ic'] = i_final_state
-    parameters['initiation']['i_start'] = i_final_state \
-        * parameters['output']['i_save_fields']
-    parameters['initiation']['t_start'] = parameters['initiation']['i_start'] \
-                                        * parameters['time_stepping']['dt']
-    dns.writeParameters(parameters, "parameters.in")
+    states = sorted(list(rundir.glob("state.*")))
+    i_final_state = int(states[-1].name[-6:])
 
-    files = list(glob.glob('*.gp'))
-    print(files)
+    parameters = dns.readParameters(rundir / "parameters.in")
+    parameters["initiation"]["ic"] = i_final_state
+    itime_final = i_final_state * parameters["output"]["i_save_fields"]
+    parameters["initiation"]["i_start"] = itime_final
+
+    stat_file = rundir / "stat.gp"
+    if Path.is_file(stat_file):
+        stats = np.loadtxt(rundir / "stat.gp")
+        times = t_final_state = stats[stats[:, 0] == itime_final]
+        if len(times) > 0:
+            t_final_state = stats[stats[:, 0] == itime_final][-1][1]
+        else:
+            t_final_state = itime_final * parameters["time_stepping"]["dt"]
+    else:
+        t_final_state = itime_final * parameters["time_stepping"]["dt"]
+
+    parameters["initiation"]["t_start"] = t_final_state
+    dns.writeParameters(parameters, rundir / "parameters.in")
+
+    files = list(rundir.glob("*.gp"))
 
     for file in files:
         with open(file, "r") as f:
             lines = f.readlines()
-            
+
         with open(file, "w") as f:
             for line in lines:
                 try:
-                    i_time = int(re.search(r'\d+', line).group())
+                    i_time = int(re.search(r"\d+", line).group())
 
-                    if i_time > parameters['initiation']['i_start']:
+                    if i_time > itime_final:
                         break
                     else:
-                        print(i_time)
                         f.write(line)
 
                 except:
-                    pass 
                     f.write(line)
 
     if not script == None:
