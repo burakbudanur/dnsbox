94ec8ada2e13b8db32286ed7da72ee6acc9fb266
Nazmi Burak Budanur <nbudanur@pks.mpg.de>
2021-09-28 10:57:08 +0200

    dnsrmstates.py reduces the number of fields to reduce the size of a run directory

---
+++ python/dnsrmstates.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import argparse
+import re
+from pathlib import Path
+from subprocess import call
+import numpy as np
+
+import dns
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Delete states in a directory keeping every n. Rename states that are kept so that the remaining are state.000000, state.000001, ..., change parameters.in accordingly.",
+    )
+    parser.add_argument("rundir", type=str, help="path to the run")
+    parser.add_argument("n", type=int, help="path to the run")
+
+    args = vars(parser.parse_args())
+
+    dnsrmstates(**args)
+
+    
+def dnsrmstates(rundir, n):
+
+    rundir = Path(rundir)
+    parameters = dns.readParameters(rundir / "parameters.in")
+    parameters["output"]["i_save_fields"] *= n
+    dns.writeParameters(parameters, rundir / "parameters.in")
+    
+    states = sorted(list(rundir.glob("state.*")))
+    perturbed_states = sorted(list(rundir.glob("perturb.*")))
+
+    if len(states) > 0:
+        for state in states:
+            i_state = int(state.name[-6:]) 
+            if i_state%n == 0:
+                state.replace(Path(f'state.{i_state//n:06}'))
+            else: 
+                state.unlink()
+
+    if len(perturbed_states) > 0:
+        for state in perturbed_states:
+            i_state = int(state.name[-6:]) 
+            if i_state%n == 0:
+                state.replace(Path(f'perturb.{i_state//n:06}'))
+            else: 
+                state.unlink()
+
+if __name__ == "__main__":
+    main()
